19dbb24408b0a12703e77d51b1e2e86e9c64d577
Akash <akashsudan555@gmail.com>
2022-07-06 13:47:44 +0530

    My first commit

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import postRoutes from "./routes/posts.js";
 import dotenv from 'dotenv';
-dotenv.config();
+
 const app=express();
+dotenv.config();
+
+
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb" ,extended:true}));
 app.use(cors());
